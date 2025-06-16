832c72c32bb39763d1411d7fe7cdb7b822e863f5
lathifjugo <muhammadlathif291002@gmail.com>
2025-06-16 22:06:56 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update
+apt-get install -y \
+    libcairo2-dev \
+    libpango1.0-dev \
+    libgdk-pixbuf2.0-dev \
+    libffi-dev \
+    libxml2-dev \
+    libxslt-dev \
+    python3-dev \
+    python3-pip \
+    python3-venv
